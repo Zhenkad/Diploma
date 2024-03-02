aaaae52414533e1a1bd2c016c3302d11c7fd7f38
Zhenkad <evgenii200127@gmail.com>
2024-03-02 20:58:37 +1000

    123

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Routes, Route } from 'react-router-dom'
 import MainPage from '../pages/MainPage';
 import Registration from '../pages/Registration';
 import Auth from '../pages/Auth';
 import Admin from '../pages/Admin';
+import { Context } from "../index";
 
 
 const AppRouter = () => {
-    const isAuth = false 
-
-    if (!isAuth){
+    const {user} = useContext(Context)
+    console.log(user)
+    if (!user.isAuth){
         return (
             <Routes>
                 <Route path = '/' element = {<MainPage />} />
