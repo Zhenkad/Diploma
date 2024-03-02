d58674752f0eef141f0b66cffd31b03c0138f468
Zhenkad <evgenii200127@gmail.com>
2024-03-02 16:14:27 +1000

    Развернул React

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,11 +2,12 @@ const Router = require('express')
 const userController = require('../controllers/userController')
 const router = new Router()
 const authMiddleware = require('../middleware/authMiddleware')
+const checkRole = require('../middleware/checkRoleMiddleware')
 
 router.post('/registration', userController.registration)
 router.post('/login', userController.login)
 router.get('/users', userController.getAll)
 router.get('/auth', authMiddleware, userController.check)
-router.delete('/user', userController.deleteUser)
+router.delete('/deletuser', checkRole('Admin'), userController.deleteUser)
 
 module.exports = router
