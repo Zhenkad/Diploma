a57cb59661c359169992fb6b3108f581e759b157
Zhenkad <evgenii200127@gmail.com>
2024-03-12 13:23:40 +1000

    Добавил таблицы с токенами и уровнями

--- server/models/models.js
+++ server/models/models.js
@@ -3,11 +3,26 @@ const {DataTypes, STRING} = require('sequelize')
 
 const User = sequelize.define('users', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    user_name: {type: DataTypes.STRING, unique: true},
-    password: {type: DataTypes.STRING},
+    user_name: {type: DataTypes.STRING, unique: true, allowNull: false},
+    password: {type: DataTypes.STRING, allowNull: false},
     role: {type: DataTypes.STRING, defaultValue: "User"}
 })
 
+const Levels = sequelize.define('levels', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false},
+})
+
+const Tokens = sequelize.define('tokens', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    token: {type: DataTypes.STRING, unique: true, allowNull: false},
+})
+
+Levels.hasMany(Tokens)
+User.hasMany(Tokens)
+
 module.exports = {
-    User
+    User,
+    Levels,
+    Tokens
 }
