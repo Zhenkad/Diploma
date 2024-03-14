855683b09601409d5103d0dd02d1fea5147a048f
Zhenkad <evgenii200127@gmail.com>
2024-03-15 00:23:56 +1000

    1212

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import {Container} from 'react-bootstrap';
+import { Container, Form } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 const Auth = () => {
     return (
-        <Container className="d-flex justify-content-center">
-            Auth
+        <Container className="d-flex justify-content-center align-items-center" style={{ height: window.innerHeight - 54 }}>
+            <Card style={{ width: 600 }} className="p-5">
+                <h2 className="m-auto">Авторизация</h2>
+                <Form className="d-flex flex-column">
+                    <Form.Control className="mt-3" placeholder="Имя пользователя..."/>
+                    <Form.Control className="mt-3" placeholder="Пароль..."/>
+                </Form>
+                <Button className="mt-3 align-self-end" variant={"outline-success"}>Войти</Button>
+            </Card>
         </Container>
     );
 };
