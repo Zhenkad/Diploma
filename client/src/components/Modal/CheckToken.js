bdeea8b5f9b7155a145fb930f35394142e0ba978
Zhenkad <evgenii200127@gmail.com>
2024-04-10 12:09:26 +1000

    Добавил валидацию

--- client/src/components/Modal/CheckToken.js
+++ client/src/components/Modal/CheckToken.js
@@ -2,21 +2,34 @@ import React, { useState } from 'react'
 import { observer } from 'mobx-react-lite'
 import { FormControl, Modal, Button } from 'react-bootstrap';
 import { passLevel } from '../../http/levelAPI';
+import { useForm } from 'react-hook-form'
 
 const CheckToken = observer(({ show, onHide, levelId, userId }) => {
     const [token, setToken] = useState('');
 
+    /**
+    * Валидация с помощью React-hook-form
+    */
+    const {
+        register,
+        formState: {errors},
+        handleSubmit
+    } = useForm()
+ 
+    /**
+     * Отправка токена на проверку
+     */
     const checkToken = async () => {
-        console.log(token)
-        console.log(levelId)
-        console.log(userId)
         try {
-            await passLevel(userId, levelId, token).then(alert("Задание выполнено")).then(window.location.reload())
+            await passLevel(userId, levelId, token)
+            alert("Задание выполнено")
+            window.location.reload()
         }
         catch (e) {
             alert(e.response.data.message)
         }
     }
+
     return (
         <Modal
             show={show}
@@ -30,10 +43,27 @@ const CheckToken = observer(({ show, onHide, levelId, userId }) => {
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <FormControl value={token} onChange={e => setToken(e.target.value)} className="mt-3 mb-3" placeholder={"Введите ключ (строка из 12 символов)"} />
+                <FormControl {...register("token",{
+                    required: "Поле обязательно к заполнению",
+                    minLength: {
+                        value: 24,
+                        message: "Длина ключа должна быть 24 символа"
+                    },
+                    maxLength: {
+                        value: 24,
+                        message: "Длина ключа должна быть 24 символа"
+                    },
+                    pattern: {
+                        value: /[A-Za-z1-9]/,
+                        message: "Ключ не должен содержать спец. символы"
+                    }
+                }
+            )} value={token} onChange={e => setToken(e.target.value)} className="mt-3 mb-3" placeholder={"Введите ключ (строка из 24 символа)"} 
+            />
+            <div>{errors?.token && <p style={{color: "red"}}>{errors?.token?.message}</p>}</div>
             </Modal.Body>
             <Modal.Footer>
-                <Button variant={"success"} onClick={checkToken}>Отправить</Button>
+                <Button variant={"success"} onClick={handleSubmit(checkToken)}>Отправить</Button>
                 <Button variant={"danger"} onClick={onHide}>Закрыть</Button>
             </Modal.Footer>
         </Modal>
--- client/src/components/Modal/CreateLevel.js
+++ client/src/components/Modal/CreateLevel.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
-import {Form} from "react-bootstrap";
-import {observer} from "mobx-react-lite"
+import { Form } from "react-bootstrap";
+import { observer } from "mobx-react-lite"
 import { createLevel } from '../../http/levelAPI';
+import { useForm } from 'react-hook-form'
 
-const CreateLevel = observer(({show, onHide}) => {
+const CreateLevel = observer(({ show, onHide }) => {
     const [name, setName] = useState('')
     const [url, setUrl] = useState('')
     const [file, setFile] = useState(null)
@@ -13,7 +14,13 @@ const CreateLevel = observer(({show, onHide}) => {
         setFile(e.target.files[0])
     }
 
-    const  addLevel = async () => {
+    const {
+        register,
+        formState: { errors },
+        handleSubmit
+    } = useForm()
+
+    const addLevel = async () => {
         const formData = new FormData()
         formData.append('name', name)
         formData.append('url', url)
@@ -35,13 +42,29 @@ const CreateLevel = observer(({show, onHide}) => {
             </Modal.Header>
             <Modal.Body>
                 <Form>
-                    <Form.Control value={name} onChange={e => setName(e.target.value)} className="mt-3 mb-3" placeholder={"Введите название испытания"}/>
-                    <Form.Control value={url} onChange={e => setUrl(e.target.value)} className="mt-3 mb-3" placeholder={"Введите порт испытания"}/>
-                    <Form.Control className="mt-3 mb-3" type="file" onChange={selectFile}/>
+
+                    <Form.Control {...register("levelName", {
+                        required: "Поле обязательно к заполнению",
+                    }
+                    )} value={name} onChange={e => setName(e.target.value)} className="mt-3 mb-3" placeholder={"Введите название испытания"} />
+                    <div>{errors?.levelName && <p style={{color: "red"}}>{errors?.levelName?.message}</p>}</div>
+
+                    <Form.Control {...register("levelUrl", {
+                        required: "Поле обязательно к заполнению",
+                    }
+                    )} value={url} onChange={e => setUrl(e.target.value)} className="mt-3 mb-3" placeholder={"Введите порт испытания"} />
+                    <div>{errors?.levelUrl && <p style={{color: "red"}}>{errors?.levelUrl?.message}</p>}</div>
+
+                    <Form.Control {...register("levelImg", {
+                        required: "Поле обязательно к заполнению",
+                    }
+                    )} className="mt-3 mb-3" type="file" onChange={selectFile} />
+                    <div>{errors?.levelImg && <p style={{color: "red"}}>{errors?.levelImg?.message}</p>}</div>
+
                 </Form>
             </Modal.Body>
             <Modal.Footer>
-                <Button variant={"success"} onClick={addLevel}>Добавить</Button>
+                <Button variant={"success"} onClick={handleSubmit(addLevel)}>Добавить</Button>
                 <Button variant={"danger"} onClick={onHide}>Закрыть</Button>
             </Modal.Footer>
         </Modal>
