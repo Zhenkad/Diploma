579e22b9e0ca46fb49d8a6c795ae6c95313b6838
Zhenkad <evgenii200127@gmail.com>
2024-04-21 22:07:11 +1000

    000

--- client/src/components/Modal/DeleteUser.js
+++ client/src/components/Modal/DeleteUser.js
@@ -6,6 +6,7 @@ import { deleteUser } from '../../http/userApi';
 const DeleteUser = observer(({ data, show, onHide }) => {
 
     const userDelete = async () => {
+        if(!window.confirm('Вы действительно хотите удалить пользователя? \n Это действие нельзя отменить!')) return;
         try {
             await deleteUser(data.id)
             alert("Пользователь удален")
@@ -31,9 +32,6 @@ const DeleteUser = observer(({ data, show, onHide }) => {
             <Modal.Body>
                 <div>
                     <p>Вы действительно хотите удалить пользователя {<b>{data.user_name}</b>}?</p>
-                </div>
-                <div class="alert alert-warning" role="alert">
-                    Это действие невозможно отменить.
                 </div>
             </Modal.Body>
             <Modal.Footer>
